7806f632496c86303fca217ed477406693846293
Marlon <marlon@verion.ind.br>
2020-02-07 12:08:53 -0300

    Adiciona paginas de login/cadastro de usuario

---
+++ EEstoque/Data/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace EEstoque.Data
+{
+    public class ApplicationUser : IdentityDbContext
+    {
+
+    }
+}
